5b436ab2e35eacce2e70a4b572ced761184225c9
204g1a0577 <112165332+204g1a0577@users.noreply.github.com>
2022-09-16 15:48:33 +0530

    Update F1.c

--- F1.c
+++ F1.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 main()
 {
 int bt[20], wt[20], tat[20], i, n;
@@ -27,5 +27,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+//getch();
 }
